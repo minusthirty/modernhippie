a1d656b0937095f75401694f141b05bb8a5a78d6
Adam Maddox <adam@adammaddox.com>
2011-12-30 18:39:38 -0800

    updated assets added admin.css manifest

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,14 @@
 Modernhippie::Application.routes.draw do
   
-  # public
-  resources :posts, :only => [:index, :show]
-  
+
   # private
   namespace :admin do
     resources :posts
   end
+  match "/admin", :to => redirect("/admin/posts")
   
+  # public
+  resources :posts, :only => [:index, :show]
+
   root :to => 'posts#index'
 end
