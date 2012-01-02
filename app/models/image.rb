e65c83bf4cff65779c5cc3cf1f8eee6b85340f77
Adam Maddox <adam@adammaddox.com>
2012-01-01 21:37:14 -0800

    fixed io closed stream error.

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,7 +1,7 @@
 class Image < ActiveRecord::Base
   
   belongs_to :post, :counter_cache => true, :touch => true
-
+  
   validates_attachment_content_type :photo, 
     :content_type => ['image/jpeg', 'image/pjpeg'], 
     :message => "has to be in jpeg format"
@@ -11,36 +11,36 @@ class Image < ActiveRecord::Base
     :unless => Proc.new {|m| m[:photo].nil?}
   
   has_attached_file :photo,
-    {:styles => { :admin_portrait  => ["82x110#", :jpg],
-                  :admin_landscape => ["165x110#", :jpg],
+    {:styles => { :admin_vertical   => ["82x110#", :jpg],
+                  :admin_horizontal => ["165x110#", :jpg],
                   
-                  :portrait     => ["245x330#", :jpg],
-                  :landscape    => ["500x330#", :jpg]}
+                  :vertical     => ["245x330#", :jpg],
+                  :horizontal   => ["500x330#", :jpg]}
                   
     }.merge(PAPERCLIP_STORAGE_OPTIONS)
-  before_photo_post_process :store_exif
+ after_photo_post_process :store_exif
   
   # return image url for orientation
   # limit to either landscape or portrait
   def oriented_photo_url(prefix=nil)
-    photo_style = self.orientation == 'landscape' ? 'landscape' : 'portrait'
+    photo_style = self.orientation == 'horizontal' ? 'horizontal' : 'vertical'
     photo_style = "#{prefix.to_s}_#{photo_style}" if prefix
     self.photo.url(photo_style.to_sym)
   end
   
   private  
   def store_exif
-    img_file = self.photo.to_file
+    img_file = self.photo.queued_for_write[:original].path
     return unless img_file
     
-    img_data = EXIFR::JPEG.new(self.photo.to_file)
+    img_data = EXIFR::JPEG.new(img_file)
     return unless img_data
     
     aspect = img_data.width / img_data.height.to_f
     self.orientation = case 
       when aspect == 1 then 'square'
-      when aspect > 1  then 'landscape'
-      when aspect < 1  then 'portrait'
+      when aspect > 1  then 'horizontal'
+      when aspect < 1  then 'vertical'
       else 
         nil
     end
