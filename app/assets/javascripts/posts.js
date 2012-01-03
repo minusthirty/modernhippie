313d7eae62780b3da797178a1568f61e34d9166b
Adam Maddox <adam@adammaddox.com>
2012-01-02 21:51:03 -0800

    ease in/out adjust

--- app/assets/javascripts/posts.js
+++ app/assets/javascripts/posts.js
@@ -12,8 +12,8 @@ function add_fields(link, association, content) {
 // home page cover image rollover
 $(function(){
   $("#posts li a").hover(function(){
-    $("img", this).stop().animate({left:"-245px"},{queue:false,duration:200});
+    $("img", this).stop().animate({left:"-245px"},{queue:false,duration:400});
   }, function() {
-    $("img", this).stop().animate({left:"0px"},{queue:false,duration:200});
+    $("img", this).stop().animate({left:"0px"},{queue:false,duration:300});
   });
 });
