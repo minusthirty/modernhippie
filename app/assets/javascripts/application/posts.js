1c82564ee8ae571baa3ad3ef54280c1226e06b3d
Adam Maddox <adam@adammaddox.com>
2012-01-22 00:41:37 -0800

    added isotope

--- app/assets/javascripts/application/posts.js
+++ app/assets/javascripts/application/posts.js
@@ -1,9 +1,29 @@
 
+
 // home page cover image rollover
 $(function(){
+  var $postList = $("ul#posts");
+
+  // initialize isotope
+  $postList.isotope({
+    itemSelector : 'li.post'
+  });
+  
+  // filter items when filter link is clicked
+  $('a.filter').click(function(){
+    var selector = $(this).attr('data-filter');
+    $postList.isotope({ filter: selector });
+    return false;
+  });
+  
+  // slider
   $("#posts li a").hover(function(){
     $("img", this).stop().animate({left:"-245px"},{queue:false,duration:400});
   }, function() {
     $("img", this).stop().animate({left:"0px"},{queue:false,duration:300});
   });
 });
+
+
+
+
